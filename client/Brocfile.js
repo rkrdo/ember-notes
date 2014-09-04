d8d56a49c27595e85cca1c7cec170ce976500022
Ricardo Cruz <ricardocruz.a89@gmail.com>
2014-09-03 23:10:45 -0500

    Updated to ember-cli 0.0.42

--- client/Brocfile.js
+++ client/Brocfile.js
@@ -2,33 +2,7 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp({
-  name: require('./package.json').name,
-
-  minifyCSS: {
-    enabled: true,
-    options: {}
-  },
-
-  getEnvJSON: require('./config/environment')
-});
-
-// Use this to add additional libraries to the generated output files.
-app.import('vendor/ember-data/ember-data.js');
-
-// If the library that you are including contains AMD or ES6 modules that
-// you would like to import into your application please specify an
-// object with the list of modules as keys along with the exports of each
-// module as its value.
-app.import('vendor/ic-ajax/dist/named-amd/main.js', {
-  'ic-ajax': [
-    'default',
-    'defineFixture',
-    'lookupFixture',
-    'raw',
-    'request',
-  ]
-});
+var app = new EmberApp();
 
 // Importing bootstrap
 app.import('vendor/bootstrap/dist/css/bootstrap.min.css');
@@ -36,6 +10,5 @@ app.import('vendor/bootstrap/dist/js/bootstrap.min.js');
 
 // Localstorage
 app.import('vendor/ember-localstorage-adapter/localstorage_adapter.js');
-
 
 module.exports = app.toTree();
