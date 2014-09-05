c72f0b019bbc75a7a58106fcdf15e292614cdf74
Ricardo Cruz <ricardocruz.a89@gmail.com>
2014-09-05 01:38:58 -0500

    Added an 'introduction' computed property to the Note model

--- client/app/models/note.js
+++ client/app/models/note.js
@@ -1,6 +1,13 @@
 var Note = DS.Model.extend({
   name: DS.attr('string'),
-  value: DS.attr('string')
+  value: DS.attr('string'),
+  introduction: function() {
+    var intro = "";
+    if(this.get('value')) {
+      intro = this.get('value').substring(0, 20);
+    }
+    return intro;
+  }.property('value')
 });
 
 export default Note;
